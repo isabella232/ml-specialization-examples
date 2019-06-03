dd6926000388bb00b67a323743f0f37fb22760a0
gad <gidutz@gmail.com>
2019-06-03 08:03:39 +0300

    removed params

--- black_friday/cmle/hyperopt/submit_hyperparam.sh
+++ black_friday/cmle/hyperopt/submit_hyperparam.sh
@@ -1,10 +1,10 @@
 # https://github.com/GoogleCloudPlatform/cloudml-samples/tree/wenzhel-sklearn/xgboost/iris
 
-PROJECT_ID=gad-playground-212407
+PROJECT_ID=<YOUR_PROJECT>
 BUCKET_ID=doitintl_black_friday
-TRAINING_PACKAGE_PATH="/Users/gad/PycharmProjects/ml-specialization-examples/black_friday/cmle/hyperopt/black_friday_hyper_trainer/"
+TRAINING_PACKAGE_PATH="<LOCAL TRAINING PATH>/black_friday_hyper_trainer/"
 MAIN_TRAINER_MODULE="black_friday_hyper_trainer.trainer"
-REGION=[REGION]
+REGION=<REGION>
 RUNTIME_VERSION=1.8
 PYTHON_VERSION=3.5
 SCALE_TIER=BASIC
@@ -16,7 +16,7 @@ gcloud ml-engine jobs submit training $JOB_NAME \
   --job-dir $JOB_DIR \
   --package-path $TRAINING_PACKAGE_PATH \
   --module-name $MAIN_TRAINER_MODULE \
-  --region us-central1 \
+  --region #$REGION \
   --runtime-version $RUNTIME_VERSION \
   --python-version $PYTHON_VERSION \
   --scale-tier BASIC \
